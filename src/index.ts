65fd77c8cf3b705f599ad77aa4b8c3edf4eef759
Atif <atif276ali@gmail.com>
2025-01-25 21:08:59 +0530

    Imported routes in index file

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// Routes
+import categoryRoutes from "./routes/category.route";
+import subcategoryRoutes from "./routes/subcategory.route.js";
+import itemRoutes from "./routes/item.route.js";
+app.use("/api/v1/categories", categoryRoutes);
+app.use("/api/v1/subcategories", subcategoryRoutes);
+app.use("/api/v1/items", itemRoutes);
 // ! DB and Server Connection
 connectDB()
   .then(() => {
