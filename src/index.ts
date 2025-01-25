142e203e34121a53504db0eab3ee9565928d5ff8
Atif <atif276ali@gmail.com>
2025-01-26 01:11:18 +0530

    chore: Reorder imports and add health check route

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 import express, { Request, Response, NextFunction } from "express";
-import connectDB from "./config/db";
 import dotenv from "dotenv";
-import ExpressResponse from "./utils/ExpressResponse.js";
+dotenv.config();
 import ExpressError from "./utils/ExpressError";
-
 import categoryRoutes from "./routes/category.route";
 import subcategoryRoutes from "./routes/subcategory.route.js";
 import itemRoutes from "./routes/item.route.js";
+import connectDB from "./config/db";
 
-dotenv.config();
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Routes
-
 app.use("/api/v1/categories", categoryRoutes);
 app.use("/api/v1/subcategories", subcategoryRoutes);
 app.use("/api/v1/items", itemRoutes);
-
+app.use("/", (req, res) => {
+  res.send("Api is Working"); //Health check route
+});
 // ! Error Handler Middleware
 app.use(
   (err: ExpressError, req: Request, res: Response, next: NextFunction) => {
